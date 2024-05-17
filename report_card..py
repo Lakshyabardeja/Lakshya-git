2daf21dd089c4e7eb8da4f8d6f0d91e503c65ea0
Lakshybardeja <lakshyabrdeja0000@gmail.com>
2024-05-17 18:31:37 +0530

    report_card

---
+++ report_card..py
@@ -0,0 +1,42 @@
+# # number = int(input("type your specific number"))
+# # if  number % 2 == 0:
+# #   print("this is an even number")
+# # else :
+# #   print("this is an odd number")
+
+
+# # age= int(input("whats ur age?"))
+# # if age == +18 :
+# #   print("Your ille")
+  
+
+# # 0 - 333 fail 
+# #  90 - 100 a+
+# # great 
+
+input("welcome to daisy international school,what is your name?")
+report_card= int(input("what is your marks?"))
+if report_card <= 33 :
+  print("fail")
+elif  report_card <= 50 :
+  print("D")
+elif report_card <= 70 :
+  print("C ")
+elif report_card <= 80 :
+  print("B")
+elif report_card <= 90 :
+  print("A")
+elif report_card <= 100 :
+  print("A+")
+else :
+  print("invalid output, please enter valid marks")
+  
+
+  #write a progrm to check weather the last digit of a number entered by a user is dibvisible by 3 or not.
+# last_digit = 0
+# number = int(input("type number"))
+# last_digit = number % 10
+# if  (last_digit % 3 == 0) :
+#   print(f"the last digit of {number} which is {last_digit} is divisible by 3")
+# else:
+#   print(f"the last digit of {number} which is {last_digit} is not divisible by 3")
