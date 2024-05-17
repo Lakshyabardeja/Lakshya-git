3b75569d423f96c3fe43246053d8a8415df16cbe
Lakshybardeja <lakshyabrdeja0000@gmail.com>
2024-05-17 18:56:45 +0530

    week days

---
+++ week_days.py
@@ -0,0 +1,60 @@
+# # # number = int(input("type your specific number"))
+# # # if  number % 2 == 0:
+# # #   print("this is an even number")
+# # # else :
+# # #   print("this is an odd number")
+
+
+
+  
+
+
+# # input("welcome to daisy international school,what is your name?")
+# # report_card= int(input("what is your marks?"))
+# # if report_card <= 0 :
+# #  print("invalid output")
+# # if report_card <= 33 :
+# #   print("fail")
+# # elif  report_card <= 50 :
+# #   print("D")
+# # elif report_card <= 70 :
+# #   print("C ")
+# # elif report_card <= 80 :
+# #   print("B")
+# # elif report_card <= 90 :
+# #   print("A")
+# # elif report_card <= 100 :
+# #   print("A+")
+# # else :
+# #   print("invalid output, please enter valid marks")
+  
+
+#   #write a progrm to check weather the last digit of a number entered by a user is dibvisible by 3 or not.
+# last_digit = 0
+# number = int(input("type number"))
+# last_digit = number % 10
+# if  (last_digit % 3 == 0) :
+#   print(f"the last digit of {number} which is {last_digit} is divisible by 3")
+# else:
+#   print(f"the last digit of {number} which is {last_digit} is not divisible by 3")
+
+#write a program to accept a no. from 1 - 7 and display the name of the day like 1 for sunday , 2 for monday and so on.
+
+number =int(input("type a number from 1 - 7"))
+if number == 1 :
+  print("sunday")
+elif number == 2 :
+  print("monday")
+elif number == 3 :
+ print("Tuesday")
+elif number == 4 :
+   print("Wednesday")
+elif number == 5 :
+ print("Thursday")
+elif number == 6 :
+ print("Friday")
+elif number == 7 :
+ print("Saturday")
+else :
+  print("no days before or after this number")
+  
